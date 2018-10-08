921e8722e6103ab1e88fe4420157c92f75e2afbe
Your Name <you@example.com>
2018-10-08 20:16:49 +0300

    veikiantis skaiciu nuskaitymas ir ju isvedimas i ekrana

--- src/lt/kaunascoding/Main.java
+++ src/lt/kaunascoding/Main.java
@@ -1,8 +1,15 @@
 package lt.kaunascoding;
 
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Labas, iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Ivestas skaicius: "+a);
     }
 }
